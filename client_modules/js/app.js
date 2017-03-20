57a9c94f89e7bc64be5480eaa02fc950da43a2cf
Amy Wieliczka <amywieliczka@gmail.com>
2017-03-20 14:11:05 -0700

    Exposed Masonry and ImagesLoaded, fixed issue where tablesorter was not getting initialized when a user landed at a search results set as opposed to performing a search

--- client_modules/js/app.js
+++ client_modules/js/app.js
@@ -1,10 +1,9 @@
 var autocomplete = require('../../node_modules/jquery-ui/ui/widgets/autocomplete');
 var tabs = require('../../node_modules/jquery-ui/ui/widgets/tabs');
 var tablesorter = require('../../node_modules/tablesorter/dist/js/jquery.tablesorter');
-var imagesLoaded = require('../../node_modules/imagesloaded/imagesloaded');
-imagesLoaded.makeJQueryPlugin( $ );
-var Masonry = require('masonry-layout');
 var datepicker = require('../../node_modules/jquery-ui/ui/widgets/datepicker');
+require('expose?imagesLoaded!../../node_modules/imagesloaded/imagesloaded');
+require('expose?Masonry!masonry-layout');
 require('expose?d3!d3');
 require('expose?xga!./ua.js');
 require("expose?$!jquery");
