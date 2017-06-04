ec56848e60028516e49deb41a11f5ac17d92739d
John Lowe <jblowe@berkeley.edu>
2017-06-04 02:11:57 -0700

    PAHMA-863: refactor permalink to not use redirects and relative URLs

--- permalinks/views.py
+++ permalinks/views.py
@@ -1,11 +1,14 @@
 __author__ = 'jblowe'
 
-from django.shortcuts import redirect
+from django import forms
 from os import path
+import logging
 
-from common.appconfig import loadConfiguration, loadFields, getParms
+from django.shortcuts import render
+from common.appconfig import loadConfiguration, loadFields
 from common import cspace # we use the config file reading function
 from cspace_django_site import settings
+from common.utils import doSearch, loginfo
 
 searchConfig = cspace.getConfig(path.join(settings.BASE_PARENT_DIR, 'config'), 'search')
 FIELDDEFINITIONS = searchConfig.get('search', 'FIELDDEFINITIONS')
@@ -14,12 +17,23 @@ FIELDDEFINITIONS = searchConfig.get('search', 'FIELDDEFINITIONS')
 prmz = loadConfiguration('common')
 prmz = loadFields(FIELDDEFINITIONS, prmz)
 
+# Get an instance of a logger, log some startup info
+logger = logging.getLogger(__name__)
+logger.info('%s :: %s :: %s' % ('permalink startup', '-', '%s | %s | %s' % (prmz.SOLRSERVER, prmz.IMAGESERVER, prmz.BMAPPERSERVER)))
+
 def get_item(request, itemid):
 
-    x = prmz
     searchfield = ''
     for i in prmz.FIELDS['Search']:
         if 'objectno' in i['fieldtype']:
             searchfield = i['name']
             break
-    return redirect('/search/search/?resultsOnly=true&displayType=full&maxresults=50&start=1&%s=%s' % (searchfield,itemid))
+    requestObject = {searchfield: itemid, 'resultsOnly': 'true'}
+    form = forms.Form(requestObject)
+
+    if form.is_valid():
+        context = {'searchValues': requestObject}
+        context = doSearch(context, prmz, request)
+
+        loginfo(logger, 'results.%s' % context['displayType'], context, request)
+        return render(request, 'search.html', context)
