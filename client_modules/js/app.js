dca4588ec061cd8353773f174d614d992d3214ab
John Lowe <jblowe@berkeley.edu>
2017-12-17 20:19:44 -0800

    NOJIRA: d3 seems not to work with webpack anymore. comment it out for now.

--- client_modules/js/app.js
+++ client_modules/js/app.js
@@ -4,7 +4,7 @@ var tablesorter = require('../../node_modules/tablesorter/dist/js/jquery.tableso
 var datepicker = require('../../node_modules/jquery-ui/ui/widgets/datepicker');
 require('expose?imagesLoaded!../../node_modules/imagesloaded/imagesloaded');
 require('expose?Masonry!masonry-layout');
-require('expose?d3!d3');
+// require('expose?d3!d3');
 require('expose?xga!./ua.js');
 require("expose?$!jquery");
 require('./PublicSearch.js');
