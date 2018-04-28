22b7ccccf158083e11b7efbc50bd4e3623f5a050
John Lowe <jblowe@berkeley.edu>
2018-04-27 20:43:37 -0700

    DJAN-98: add watermark_size parameter for watermarking. improve parameter checking

--- imageserver/views.py
+++ imageserver/views.py
@@ -33,10 +33,20 @@ try:
     watermark = True if (watermark.lower() in 'true yes on') else False
     watermark_image = config.get('info', 'watermarkimage')
     watermark_transparency = float(config.get('info', 'watermarktransparency'))
+    if watermark_transparency > 1.0 or watermark_transparency < 0.0:
+        logger.info('watermarktransparency value is invalid: %s' % watermark_transparency)
+        raise
+    watermark_percent = float(config.get('info', 'watermarkpercent'))
+    if watermark_percent > 1.0 or watermark_percent < 0.0:
+        logger.info('watermarkpercent value is invalid: %s' % watermark_percent)
+        raise
+    if not path.isfile(watermark_image):
+        logger.info('could not find watermark image: %s' % watermark_image)
+        raise
     logger.info('watermarking enabled. transparency: %s, image: %s' % (watermark_transparency, watermark_image))
 except:
     watermark = False
-    logger.info('%s' % 'watermarking NOT enabled.')
+    logger.info('%s' % 'configuration problem. watermarking NOT enabled.')
 
 imageunavailable = config.get('info', 'imageunavailable')
 unavailable_mime_type = 'jpg'
@@ -63,6 +73,14 @@ from wand.image import Image
 def add_watermark(image1, image2):
     with Image(blob=image1) as background:
         with Image(filename=image2) as watermark:
+            background_size = background.size
+            watermark_size = watermark.size
+            # (width, height): is the width bigger than the height?
+            if background_size[0] > background_size[1]:
+                background_to_watermark_ratio = float(background_size[0]) / watermark_size[0]
+            else:
+                background_to_watermark_ratio = float(background_size[1]) / watermark_size[1]
+            watermark.transform(resize='%s%%' % int(100.0 * background_to_watermark_ratio * watermark_percent))
             background.watermark(image=watermark, transparency=watermark_transparency)
             return background.make_blob()
 
@@ -128,6 +146,7 @@ def get_image(request, image):
         try:
             data = add_watermark(data, watermark_image)
         except:
+            raise
             logger.info('%s :: %s :: %s' % ('watermark failed', '-', url))
     elapsedtime = time.time() - elapsedtime
     logger.info('%s :: %s :: %s' % (msg, '-', '%s :: %8.3f seconds' % (image, elapsedtime)))
