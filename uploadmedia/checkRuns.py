700801a5c4f65eb25385034e60ea763c39678ae1
John Lowe <jblowe@berkeley.edu>
2018-12-05 07:36:36 -0800

    UJ-44: refactored and improved BMU run status report

--- uploadmedia/checkRuns.py
+++ uploadmedia/checkRuns.py
@@ -1,17 +1,27 @@
 import sys, os
 
 
-def checkJobs(jobs, joberrors):
+def pluralize(n, w):
+    if n == 1:
+        return '%s %s' % (n, w)
+    else:
+        return '%s %ss' % (n, w)
+
+
+def checkJobs(jobs, joberrors, report_type):
     totals = {}
 
-    print "number of jobs found: %s " % len(jobs.keys())
+    print "Bulk Media Uploader Progress Report\n"
+    print "job directory: %s" % DIR
 
-    columnheaders = 'step1 original processed inprogress discrepancy logerrors'.split(' ')
+    columnheaders = 'step1 original processed inprogress discrepancy error messages seen'.split(' ')
     print
     # print "\t".join(columnheaders)
     for c in columnheaders: totals[c] = 0
 
-    for job in sorted(jobs.keys(), reverse=True):
+    output_lines = []
+    for i, job in enumerate(sorted(jobs.keys(), reverse=True)):
+
         steps = jobs[job]
 
         try:
@@ -20,39 +30,48 @@ def checkJobs(jobs, joberrors):
             try:
                 steps['discrepancy'] = steps['original']
             except:
-                print 'no original for %s' % job
+                steps['discrepancy'] = -1
         # if steps['discrepancy'] == 0: del steps['discrepancy']
         if job in joberrors:
-            steps['logerrors'] = joberrors[job]
+            steps['error messages seen'] = joberrors[job]
         else:
-            steps['logerrors'] = 0
+            steps['error messages seen'] = 0
 
-        print job + ":\t",
+        output_line = job + ":\t"
 
         if steps['discrepancy'] == 0:
-            print '%s images ingested' % steps['original'],
+            output_line += '%s ingested' % pluralize(steps['original'], 'image')
         else:
             try:
-                print '%s images uploaded, %s ingested, %s problems' % (
-                steps['original'], steps['processed'], steps['discrepancy']),
+                output_line += '%s uploaded, %s ingested, %s' % (
+                    pluralize(steps['original'], 'image'), pluralize(steps['processed'], 'image'),
+                    pluralize(steps['discrepancy'], 'problem'))
             except:
-                print 'run files incomplete', steps,
-        if steps['logerrors'] != 0:
-            print '%s error messages seen in trace.log' % (steps['logerrors']),
-        print
+                if 'check' in steps:
+                    output_line += 'job failed qc check; %s attempted' % pluralize(steps['check'], 'image')
+                elif 'step1' in steps:
+                    output_line += 'job pending, %s' % pluralize(steps['step1'], 'image')
+                else:
+                    output_line += 'run files incomplete %s' % steps
+        if steps['error messages seen'] != 0:
+            output_line += '%s error messages seen in trace.log' % (steps['error messages seen'])
         for step in columnheaders:
-            # print step."\t"
             if step in steps:
-                #    print '%s\t' % steps[step],
                 totals[step] += steps[step]
-            # else:
-            #    print '\t',
-        # print
 
-    print "totals\t",
+        if i >= 20 and report_type == 'summary':
+            pass
+        else:
+            output_lines.append(output_line)
+
+    if report_type == 'summary': print 'most recent %s jobs:\n' % len(output_lines)
+    print '\n'.join(output_lines)
+
+    print "\ntotal number of jobs found: %s " % len(jobs.keys())
+    print "\ngrand totals:\n\n",
     for step in columnheaders:
         if totals[step] != 0:
-            print '%s %s, ' % (step, totals[step]),
+            print '%s %s ' % (step, totals[step])
     print
 
 
@@ -65,26 +84,27 @@ def checkMissing(images, missing):
             for step in steps:
                 if step in 'processed|step1|inprogress'.split('|'): isMissing = False
 
-    if isMissing: print "name ::: \tmissingname\n"
+    if isMissing: print name, '::: \tmissingname\n'
 
 
-def checkDuplicates(images, duplicates, missing):
-    for name in duplicates:
+def checkDuplicates(images, duplicates):
+    for name in sorted(duplicates):
         if duplicates[name] > 1: print '%s duplicated %s times' % (name, duplicates[name])
     print
 
 
 def checkCsids(csids):
-    for name in csids:
+    for name in sorted(csids):
         print name + "\t",
         CSIDlist = csids[name]
-        for type in 'objectnumber blob media object'.split(' '):
-            print CSIDlist[type],
+        print CSIDlist['objectnumber'] + '\t',
+        for type in 'blob media object'.split(' '):
+            print ','.join(CSIDlist[type]),'\t',
         print
 
 
 def checkSteps(images):
-    for name in images:
+    for name in sorted(images):
         print name + "\t",
         runs = images[name]
         for run in runs:
@@ -96,7 +116,7 @@ def checkSteps(images):
 
 
 def usage():
-    print "usage: python checkRuns.py <directory> <jobs missing duplicates images csids> [yyyy-mm-dd-hh-mm-ss]"
+    print "usage: python checkRuns.py <directory> <jobs missing duplicates images csids> <full summary>"
 
 
 ########## Main ##############
@@ -110,12 +130,18 @@ errors = {}
 csids = {}
 JOB = {}
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 4:
     usage()
     sys.exit(1)
 
 if (sys.argv[1]):  # if we have a single job, just do stats for it..
     JOB = sys.argv[1]
+
+if (sys.argv[3] in ['full', 'summary']):
+    report_type = sys.argv[3]
+else:
+    print 'report type must be either "full" or "summary".'
+    sys.exit(1)
 
 files = []
 
@@ -125,27 +151,69 @@ for filename in os.listdir(DIR):
     # filename = filename.replace('.csv','')
     (run, step, extension) = filename.split('.')
     for i, line in enumerate(FH.readlines()):
+        line = line.strip()
         objectCSID = ''
         if i == 0: continue  # skip header rows
-        if step == 'processed':
-            try:
-                (name, size, objectnumber, date, creator, contributor, rightsholder, imagenumber, handling, approvedforweb,
-                mediaCSID, objectCSID, blobCSID) = line.split('\t')
-            except:
-                (name, size, objectnumber, date, creator, contributor, rightsholder, imagenumber, handling, approvedforweb,
-                mediaCSID, objectCSID) = line.split('\t')
-                blobCSID = 'not provided'
-        elif step == 'original' or step == 'step1':
-            try:
-                (name, size, objectnumber, date, creator, contributor, rightsholder, imagenumber, handling,
-                 approvedforweb, description) = line.split('|')
-            except:
+        try:
+            if step == 'processed':
+                # sorry there are 4 different formats of processed files at this point...
                 try:
                     (name, size, objectnumber, date, creator, contributor, rightsholder, imagenumber, handling,
-                     approvedforweb) = line.split('|')
+                     approvedforweb, mediaCSID, objectCSID, blobCSID) = line.split('\t')
+                    description = 'not provided'
                 except:
-                    print 'skipped', filename, line
-                    continue
+                    try:
+                        (name, size, objectnumber, date, creator, contributor, rightsholder, imagenumber, handling,
+                         approvedforweb, mediaCSID, objectCSID) = line.split('\t')
+                        blobCSID = 'not provided'
+                    except:
+                        try:
+                            (name, size, objectnumber, date, creator, contributor, rightsholder, imagenumber, handling,
+                             approvedforweb, description, mediaCSID, objectCSID, blobCSID) = line.split('\t')
+                        except:
+                            try:
+                                (name, size, objectnumber, date, creator, contributor, rightsholder, imagenumber,
+                                 handling, approvedforweb, copyright, imagetype, type, source, description, mediaCSID,
+                                 objectCSID, blobCSID) = line.split('\t')
+                            except:
+                                try:
+                                    (name, size, objectnumber, date, creator, contributor, rightsholder, imagenumber,
+                                     handling, approvedforweb, copyright, imagetype, type, source, description,
+                                     mediaCSID, objectCSID, blobCSID) = line.split('\t')
+                                except:
+                                    try:
+                                        (name, size, objectnumber, date, creator, contributor, rightsholder,
+                                         imagenumber, handling, approvedforweb, copyright, imagetype, source, locality,
+                                         mediaCSID, objectCSID, blobCSID) = line.split('\t')
+                                    except:
+                                        try:
+                                            (name, size, objectnumber, date, creator, contributor, rightsholder,
+                                             imagenumber, handling, approvedforweb, copyright, imagetype, source,
+                                             locality, mediaCSID, objectCSID) = line.split('\t')
+                                        except:
+                                            raise
+            elif step == 'original' or step == 'step1':
+                try:
+                    (name, size, objectnumber, date, creator, contributor, rightsholder, imagenumber, handling,
+                     approvedforweb, description) = line.split('|')
+                except:
+                    try:
+                        (name, size, objectnumber, date, creator, contributor, rightsholder, imagenumber, handling,
+                         approvedforweb) = line.split('|')
+                    except:
+                        try:
+                            (name, size, objectnumber, date, creator, contributor, rightsholder, imagenumber, handling,
+                             approvedforweb, copyright, imagetype, type, source, description) = line.split('|')
+                        except:
+                            try:
+                                (name, size, objectnumber, date, creator, contributor, rightsholder, imagenumber,
+                                 handling, approvedforweb, copyright, imagetype, source, locality) = line.split('|')
+                            except:
+                                print 'skipped', filename, line
+                                continue
+        except:
+            print 'problem with file %s' % filename
+            continue
         if objectCSID == 'not found': continue
         if not run in jobs: jobs[run] = {}
         if not step in jobs[run]: jobs[run][step] = 0
@@ -186,8 +254,7 @@ for filename in os.listdir(DIR):
             errors[name] += error
 
 if (sys.argv[2] == 'jobs'):
-    print "Directory: DIR\n\n"
-    checkJobs(jobs, joberrors)
+    checkJobs(jobs, joberrors, report_type)
 
 elif (sys.argv[2] == 'missing'):
     checkMissing(images, missing)
